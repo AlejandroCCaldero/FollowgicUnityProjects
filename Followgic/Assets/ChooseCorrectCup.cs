706163787045cdabe50a206b98294fc804764f0a
AlejandroCanoCaldero <alejandrocano.caldero@gmail.com>
2021-07-13 11:43:40 +0200

    updated cups game

---
+++ Followgic/Assets/ChooseCorrectCup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseCorrectCup : MonoBehaviour
+{
+    public CupsMovement game;
+
+    void OnMouseDown() 
+    {
+        if(!game.isMoving())
+        {
+            game.score = game.score + 1;
+
+            FindObjectOfType<Manager>().LevelUp();
+        }
+    }
+}
---
+++ Followgic/Assets/ChooseWrongCup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseWrongCup : MonoBehaviour
+{
+    public CupsMovement game;
+
+    void OnMouseDown()
+    {
+        if(!game.isMoving())
+        {
+            Debug.Log("You failed!!");
+            game.score = 0;
+
+            FindObjectOfType<Manager>().GameOver();
+        }
+    }
+}
